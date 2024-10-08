4f8ec0e25d892cfd59e17d6218a6b48b04215d8b
Tuonellla <sofya.tchebykina@yandex.ru>
2024-10-08 22:42:33 +0400

    Task 1 (X and Y): IdentityFunctionTest.java updated

--- sizar-ooplabs/src/test/java/ru/ssau/tk/sizar/ooplabs/Lab2/functions/IdentityFunctionTest.java
+++ sizar-ooplabs/src/test/java/ru/ssau/tk/sizar/ooplabs/Lab2/functions/IdentityFunctionTest.java
@@ -1,27 +1,15 @@
 package ru.ssau.tk.sizar.ooplabs.Lab2.functions;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class IdentityFunctionTest {
+    @Test
     void testApply() {
         IdentityFunction function = new IdentityFunction();
-        if (0.0 == function.apply(0.0)){
-            System.out.println("Test 1 passed");
-        }
-        if (1.0 == function.apply(1.0)){
-            System.out.println("Test 2 passed");
-        }
-        if (-1.0 == function.apply(-1.0)){
-            System.out.println("Test 3 passed");
-        }
-        if (Double.POSITIVE_INFINITY == function.apply(Double.POSITIVE_INFINITY)){
-            System.out.println("Test 4 passed");
-        }
-        if (Double.NEGATIVE_INFINITY == function.apply(Double.NEGATIVE_INFINITY)){
-            System.out.println("Test 5 passed");
-        }
-        if (Double.NaN == function.apply(Double.NaN)){
-            System.out.println("Test 6 passed");
-        }
+        Assertions.assertEquals(0.0, function.apply(0.0));
+        Assertions.assertEquals(-1.0, function.apply(-1.0));
+        Assertions.assertEquals(Double.POSITIVE_INFINITY, function.apply(Double.POSITIVE_INFINITY));
+        Assertions.assertEquals(Double.NEGATIVE_INFINITY, function.apply(Double.NEGATIVE_INFINITY));
     }
 }
