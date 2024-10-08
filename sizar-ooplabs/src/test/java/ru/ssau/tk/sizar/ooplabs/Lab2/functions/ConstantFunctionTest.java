c6b3c1d78a7d18801f30c2af2922b3106cd70225
BogomolovaK <bogomolovaka04@gmail.com>
2024-10-09 00:16:53 +0400

    Task 1 (Y): ConstantFunctionTest

--- sizar-ooplabs/src/test/java/ru/ssau/tk/sizar/ooplabs/Lab2/functions/ConstantFunctionTest.java
+++ sizar-ooplabs/src/test/java/ru/ssau/tk/sizar/ooplabs/Lab2/functions/ConstantFunctionTest.java
@@ -10,7 +10,5 @@ class ConstantFunctionTest {
         Assertions.assertEquals(4.3, function.apply(0.0));
         Assertions.assertEquals(4.3, function.apply(Double.POSITIVE_INFINITY));
         Assertions.assertEquals(4.3, function.apply(Double.NEGATIVE_INFINITY));
-
     }
-
 }
