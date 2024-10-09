949e2f12058e9904f02aa83b48dd6f162b307284
BogomolovaK <bogomolovaka04@gmail.com>
2024-10-09 17:43:10 +0400

    Task 1 (Y): AbstractTabulatedFunction class created

---
+++ sizar-ooplabs/src/main/java/ru/ssau/tk/sizar/ooplabs/Lab2/functions/AbstractTabulatedFunction.java
@@ -0,0 +1,33 @@
+package ru.ssau.tk.sizar.ooplabs.Lab2.functions;
+
+public abstract class AbstractTabulatedFunction implements TabulatedFunction{
+    protected int count;
+    // Метод поиска индекса x
+    protected abstract int floorIndexOfX(double x);
+    // Метод экстраполяции слева
+    protected abstract double extrapolateLeft(double x);
+    // Метод экстраполяции справа
+    protected abstract double extrapolateRight(double x);
+    // Метод интерполяции с указанием индекса интервала
+    protected abstract double interpolate(double x, int floorIndex);
+    // Интерполяция
+    protected double interpolate(double x, double leftX, double rightX, double leftY, double rightY){
+        return leftY + (x - leftX) * (rightY - leftY) / (rightX - leftX);
+    }
+
+    public double apply(double x){
+        if (x < leftBound()) {
+            return extrapolateLeft(x);
+        } else if (x > rightBound()) {
+            return extrapolateRight(x);
+        } else {
+            int index = indexOfX(x);
+            if (index != -1) {
+                return getY(index);
+            } else {
+                int floorIndex = floorIndexOfX(x);
+                return interpolate(x, floorIndex);
+            }
+        }
+    }
+}
