47165a0d77aef22b31f2a49d3dc90e5e7e91e2e4
Tuonellla <sofya.tchebykina@yandex.ru>
2024-10-09 14:29:20 +0400

    Task 2 (X): TabulatedFunction.java created

---
+++ sizar-ooplabs/src/test/java/ru/ssau/tk/sizar/ooplabs/Lab2/functions/TabulatedFunction.java
@@ -0,0 +1,21 @@
+package ru.ssau.tk.sizar.ooplabs.Lab2.functions;
+
+public interface TabulatedFunction extends MathFunction{
+    //Метод получения количества табулированных значений:
+    int getCount();
+    //Метод, получающий значение аргумента x по номеру индекса:
+    double getX(int index);
+    //Метод, получающий значение y по номеру индекса:
+    double getY(int index);
+    //Метод, задающий значение y по номеру индекса:
+    void setY(int index, double value);
+    //Метод, возвращающий индекс аргумента x. Предполагается, что все x различны. Если такого x в таблице нет, то необходимо вернуть -1:
+    int indexOfX(double x);
+    //Метод, возвращающий индекс первого вхождения значения y. Если такого y в таблице нет, то необходимо вернуть -1:
+    int indexOfY(double y);
+    //Метод, возвращающий самый левый x:
+    double leftBound();
+    //Метод, возвращающий самый правый x:
+    double rightBound();
+
+}
