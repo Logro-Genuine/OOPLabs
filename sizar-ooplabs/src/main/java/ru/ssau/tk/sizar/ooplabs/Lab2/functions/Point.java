96425ce38f2c179e5bc084d377f23c26892546ad
Tuonellla <sofya.tchebykina@yandex.ru>
2024-10-30 18:02:16 +0400

    Task 1: Point.java added

---
+++ sizar-ooplabs/src/main/java/ru/ssau/tk/sizar/ooplabs/Lab2/functions/Point.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.sizar.ooplabs.Lab2.functions;
+
+public class Point {
+    public final double x;
+    public final double y;
+    Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+}
