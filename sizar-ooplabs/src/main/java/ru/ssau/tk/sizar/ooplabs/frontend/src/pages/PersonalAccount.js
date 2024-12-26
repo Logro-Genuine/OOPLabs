bed7e29bdd1d2d83dcb375dabc57d8b383b00c6b
BogomolovaK <bogomolovaka04@gmail.com>
2024-12-26 18:26:02 +0400

    lad 7

--- sizar-ooplabs/src/main/java/ru/ssau/tk/sizar/ooplabs/frontend/src/pages/PersonalAccount.js
+++ sizar-ooplabs/src/main/java/ru/ssau/tk/sizar/ooplabs/frontend/src/pages/PersonalAccount.js
@@ -11,17 +11,10 @@ const PersonalAccount = () => {
             <Row className="mb-5">
                 <Col>
                     <div className="p-5 bg-light rounded-3">
-                        <h1 className="display-4">Добро пожаловать!</h1>
                         <p className="lead">
-                            Это наше приложение для работы с табулированными функциями.
                             Здесь вы можете создавать функции, просматривать их графики,
                             выполнять операции над ними и многое другое.
                         </p>
-                        <hr className="my-4" />
-                        <p>Воспользуйтесь возможностями приложения уже сейчас!</p>
-                        <Button as={Link} to="/functions" variant="primary" size="lg">
-                            Перейти к списку функций
-                        </Button>
                     </div>
                 </Col>
             </Row>
@@ -33,7 +26,7 @@ const PersonalAccount = () => {
                         <Card.Body>
                             <Card.Title>Создание функций</Card.Title>
                             <Card.Text>
-                                Введите данные точек вашей функции и сохраните её в базе данных.
+                                В этом разделе вы можете создать функцию и сохраните её в базе данных.
                                 Затем вы сможете просмотреть её график.
                             </Card.Text>
                             <Button as={Link} to="/create-function" variant="success">
@@ -42,13 +35,27 @@ const PersonalAccount = () => {
                         </Card.Body>
                     </Card>
                 </Col>
+                <Row>
+                                <Col md={4}>
+                                    <Card className="mb-4 shadow-sm">
+                                        <Card.Body>
+                                            <Card.Title>Просмотр функций</Card.Title>
+                                            <Card.Text>
+                                                Здесь вы можете просмотреть функции, созданные в этом приложении.
+                                            </Card.Text>
+                                            <Button as={Link} to="/functions" variant="success">
+                                                К списку функций
+                                            </Button>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
                 <Col md={4}>
                     <Card className="mb-4 shadow-sm">
                         <Card.Body>
                             <Card.Title>Список функций</Card.Title>
                             <Card.Text>
-                                Просматривайте уже созданные функции, фильтруйте их по названию,
-                                удаляйте ненужные и открывайте для просмотра графиков.
+                                Просматривайте уже созданные функции
+                                и открывайте для просмотра графиков.
                             </Card.Text>
                             <Button as={Link} to="/functions" variant="primary">
                                 Посмотреть список
